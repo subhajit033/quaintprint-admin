865fe46707eb73b45171059f239461ec5d197bfd
Subhajit Kundu <kundusubhajit73@gmail.com>
2024-09-03 01:16:00 +0530

    route changed to api route

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://quaint-print-server.onrender.com/',
+  baseURL: 'https://quaint-print-server.onrender.com/api/v1',
   withCredentials: true,
 });
 
